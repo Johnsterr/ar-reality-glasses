0f3e5880a004045ec608cc4cf8c2c6580f68e42e
Evgeniy Pashko <evgpashko92@gmail.com>
2022-01-20 19:56:38 +0700

    Complete project

--- AR Reality Glasses/ContentView.swift
+++ AR Reality Glasses/ContentView.swift
@@ -17,6 +17,44 @@ struct ContentView : View {
 
 struct ARViewContainer: UIViewRepresentable {
     
+    func createBox() -> Entity {
+        // Create mesh (geometry)
+        let mesh = MeshResource.generateBox(size: 0.2)
+        
+        // Create entity based on mesh
+        let entity = ModelEntity(mesh: mesh)
+        
+        return entity
+    }
+    
+    func createCircle(x: Float = 0, y: Float = 0, z: Float = 0) -> Entity {
+        // Create circle mesh
+        let circle = MeshResource.generateBox(size: 0.05, cornerRadius: 0.025)
+        
+        // Create material
+        let material = SimpleMaterial(color: .gray, isMetallic: false)
+        
+        // Create entity
+        let circleEntity = ModelEntity(mesh: circle, materials: [material])
+        circleEntity.position = SIMD3(x, y, z)
+        circleEntity.scale.x = 1.1
+        circleEntity.scale.z = 0.01
+        
+        return circleEntity
+    }
+    
+    func createSphere(x: Float = 0, y: Float = 0, z: Float = 0, color: UIColor = .orange, radius: Float = 1) -> Entity {
+        // Create sphere mesh
+        let sphere = MeshResource.generateSphere(radius: radius)
+        let material = SimpleMaterial(color: color, isMetallic: true)
+        
+        // Create sphere entity
+        let sphereEntity = ModelEntity(mesh: sphere, materials: [material])
+        sphereEntity.position = SIMD3(x, y, z)
+        
+        return sphereEntity
+    }
+    
     func makeUIView(context: Context) -> ARView {
         // Create ARView
         let arView = ARView(frame: .zero)
@@ -33,6 +71,17 @@ struct ARViewContainer: UIViewRepresentable {
         
         // Run face tracking session
         arView.session.run(configuration, options: [])
+        
+        // Create face anchor
+        let faceAnchor = AnchorEntity(.face)
+        
+        // Add box to the face anchor
+        faceAnchor.addChild(createCircle(x: 0.035, y: 0.025, z: 0.058))
+        faceAnchor.addChild(createCircle(x: -0.035, y: 0.025, z: 0.058))
+        faceAnchor.addChild(createSphere(z: 0.058, radius: 0.025))
+        
+        // Add face anchor to the scene
+        arView.scene.anchors.append(faceAnchor)
         
         return arView
     }
